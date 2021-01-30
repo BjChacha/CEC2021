4f9fcd7e26b015883f65dff2f2c40ee8aefab198
BjChacha <bjchacha@outlook.com>
2021-01-30 20:03:47 +0800

    add log population function in utils.

---
+++ src/etmo/util/logging/LogPopulation.java
@@ -0,0 +1,46 @@
+package etmo.util.logging;
+
+import etmo.core.ProblemSet;
+import etmo.core.Solution;
+import etmo.core.SolutionSet;
+
+import java.io.File;
+
+public class LogPopulation {
+    public static void LogPopulation(String algoName, SolutionSet population, ProblemSet problemSet, int eval, boolean isMulTask){
+        File folder = new File("D:\\_r\\EA\\ETMO\\MTO-cec2021-\\datas\\" + algoName);
+        if (!folder.exists() && !folder.isDirectory()) {
+            folder.mkdirs();
+        }
+
+        if (isMulTask) {
+            int taskNum = problemSet.size();
+            SolutionSet[] resPopulation = new SolutionSet[taskNum];
+            for (int i = 0; i < taskNum; i++)
+                resPopulation[i] = new SolutionSet();
+
+            for (int i = 0; i < population.size(); i++) {
+                Solution sol = population.get(i);
+
+                int pid = sol.getSkillFactor();
+
+                int start = problemSet.get(pid).getStartObjPos();
+                int end = problemSet.get(pid).getEndObjPos();
+
+                Solution newSolution = new Solution(end - start + 1);
+
+                for (int k = start; k <= end; k++)
+                    newSolution.setObjective(k - start, sol.getObjective(k));
+
+                resPopulation[pid].add(newSolution);
+            }
+            for (int k = 0; k < taskNum; k++)
+                resPopulation[k].printObjectivesToFile(algoName + "\\" + algoName + "_" + problemSet.get(k).getNumberOfObjectives() + "Obj_" +
+                        problemSet.get(k).getName() + "_" + problemSet.get(k).getNumberOfVariables() + "D" + eval + ".txt");
+        }
+        else{
+            population.printObjectivesToFile(algoName + "\\" + algoName + "_"+problemSet.get(0).getNumberOfObjectives()+"Obj_"+
+                    problemSet.get(0).getName()+ "_" + problemSet.get(0).getNumberOfVariables() + "D" + eval + ".txt");
+        }
+    }
+}
