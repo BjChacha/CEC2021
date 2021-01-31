1d24bf6f54713d6a7ab7cb9228d6f2b26e23f5d6
BjChacha <bjchacha@outlook.com>
2021-01-31 20:04:35 +0800

    compatible with sub-population mode.

--- src/etmo/util/logging/LogPopulation.java
+++ src/etmo/util/logging/LogPopulation.java
@@ -7,6 +7,7 @@ import etmo.core.SolutionSet;
 import java.io.File;
 
 public class LogPopulation {
+    // Single Population
     public static void LogPopulation(String algoName, SolutionSet population, ProblemSet problemSet, int eval, boolean isMulTask){
         File folder = new File("D:\\_r\\EA\\ETMO\\MTO-cec2021-\\datas\\" + algoName);
         if (!folder.exists() && !folder.isDirectory()) {
@@ -41,6 +42,30 @@ public class LogPopulation {
         else{
             population.printObjectivesToFile(algoName + "\\" + algoName + "_"+problemSet.get(0).getNumberOfObjectives()+"Obj_"+
                     problemSet.get(0).getName()+ "_" + problemSet.get(0).getNumberOfVariables() + "D" + eval + ".txt");
+        }
+    }
+
+    // Sub-population
+    public static void LogPopulation(String algoName, SolutionSet[] population, ProblemSet problemSet, int eval){
+        int taskNum = problemSet.size();
+        SolutionSet resPopulation[] = new SolutionSet[taskNum];
+        for (int k = 0; k < taskNum; k++) {
+            resPopulation[k] = new SolutionSet();
+            for (int i = 0; i < population[k].size(); i++) {
+                Solution sol = population[k].get(i);
+
+                int start = problemSet.get(k).getStartObjPos();
+                int end = problemSet.get(k).getEndObjPos();
+
+                Solution newSolution = new Solution(end - start + 1);
+
+                for (int kk = start; kk <= end; kk++)
+                    newSolution.setObjective(kk - start, sol.getObjective(kk));
+
+                resPopulation[k].add(newSolution);
+            }
+            resPopulation[k].printObjectivesToFile("MaTDE\\" + "MaTDE_"+problemSet.get(k).getNumberOfObjectives()+"Obj_"+
+                    problemSet.get(k).getName()+ "_" + problemSet.get(k).getNumberOfVariables() + "D" + eval + ".txt");
         }
     }
 }
