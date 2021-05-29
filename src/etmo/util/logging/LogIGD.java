b9a0d1ea67e10962c5162230a6f0a79110d19335
Chacha <bjchacha@outlook.com>
2021-05-29 09:50:49 +0800

    add LogIGD function

--- src/etmo/util/logging/LogIGD.java
+++ src/etmo/util/logging/LogIGD.java
@@ -3,23 +3,29 @@ package etmo.util.logging;
 import etmo.core.Solution;
 import etmo.util.Configuration;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.Arrays;
 
 public class LogIGD {
-    public static void LogIGD(String fileName, double[] igds){
+    public static void LogIGD(String algoName, int problemNum, double[][] igds){
+        String folderPath = "D:\\_r\\EA\\ETMO\\MTO-cec2021-\\data\\IGDs\\" + algoName;
+        File folder = new File(folderPath);
+        if (!folder.exists() && !folder.isDirectory()) {
+            folder.mkdirs();
+        }
+
+        int taskNum = igds.length;
+        int times = igds[0].length;
+        String filePath = folderPath + "\\" + algoName + "_ETMOF" + problemNum + "_" + times + ".txt";
         try {
             /* Open the file */
-            fileName = "data//" + fileName;
-            FileOutputStream fos = new FileOutputStream(fileName);
+            FileOutputStream fos = new FileOutputStream(filePath);
             OutputStreamWriter osw = new OutputStreamWriter(fos);
             BufferedWriter bw = new BufferedWriter(osw);
 
-            for (double igd : igds) {
+            for (double[] igd : igds) {
                 // if (this.vector[i].getFitness()<1.0) {
-                bw.write(Double.toString(igd));
+                bw.write(Arrays.toString(igd));
                 bw.newLine();
                 // }
             }
