f040e58f72dad1069cc3c06cbe9fd87238260da6
BjChacha <bjchacha@outlook.com>
2021-09-20 20:25:34 +0800

    add new distance measurement: Wasserstein Distance

--- src/main/java/etmo/util/math/Distance.java
+++ src/main/java/etmo/util/math/Distance.java
@@ -12,4 +12,23 @@ public class Distance {
         }
         return Math.pow(distance, 1/d);
     }
+
+    public static double getWassersteinDistance(double[][] p1, double[][] p2) {
+        double distance = 0;
+        int size = p1.length;
+        double[][] distanceMat = new double[size][size];
+        for (int i = 0; i < size - 1; i++){
+            distanceMat[i][i] = Double.MAX_VALUE;
+            for (int j = i + 1; j < size; j++){
+                distanceMat[i][j] = distanceMat[j][i] = getDistance(p1[i], p2[j]);
+            }
+        }
+
+        int[] assignment = new HungarianAlgorithm(distanceMat).execute();
+        for (int i = 0; i < size; i++){
+            distance += distanceMat[i][assignment[i]];
+        }
+
+        return distance / size;
+    }
 }
