a579b9e32a9fc939acaef202ef7a31deec265d48
BjChacha <bjchacha@outlook.com>
2021-10-20 15:01:24 +0800

    experiments

--- src/main/java/etmo/metaheuristics/matmy3/MaTMY3_Gaussian_main.java
+++ src/main/java/etmo/metaheuristics/matmy3/MaTMY3_Gaussian_main.java
@@ -31,18 +31,18 @@ public class MaTMY3_Gaussian_main {
 
     static final int PLOT_TASK_ID = 31;
 
-    static final Benchmark BENCHMARK_TYPE = Benchmark.WCCI2020;
+    static final Benchmark BENCHMARK_TYPE = Benchmark.CEC2019;
     static final int PROBLEM_START = 1;
-    static final int PROBLEM_END = 10;
-    static final int PROBLEM_REPEAT_TIME = 10;
+    static final int PROBLEM_END = 6;
+    static final int PROBLEM_REPEAT_TIME = 1;
 
-    static final boolean IGD_LOG = true;
+    static final boolean IGD_LOG = false;
     static final boolean IGD_PRINT = true;
     static final boolean PLOTTING = false;
 
-    static final String ALGO_NAME = "MaTMY3_SBX_PM0.5_Gau0.5_(0.5CMD(noSIM)_0.5EliteClosest)";
+    static final String ALGO_NAME = "MaTMY3_SBX_PM0.5_Gau0.5_(0.5CMD(noSIM)_0.5EliteClosest)_MultiVarGuassian";
 
-    enum Benchmark { CEC2021, CEC2017, WCCI2020 }
+    enum Benchmark { CEC2021, CEC2017, CEC2019, WCCI2020 }
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException, JMException {
         ProblemSet problemSet;
@@ -144,6 +144,7 @@ public class MaTMY3_Gaussian_main {
         algorithm.setInputParameter("transferProbability", TRANSFER_PROBABILITY);
         algorithm.setInputParameter("mutationProbability", MUTATION_PROBABILITY);
         algorithm.setInputParameter("plotTaskID", PLOT_TASK_ID);
+        algorithm.setInputParameter("elitePartition", 0.5);
 
         parameters = new HashMap<>();
         parameters.put("probability", 1.0);
@@ -197,6 +198,12 @@ public class MaTMY3_Gaussian_main {
             case WCCI2020:
                 problemSet = (ProblemSet) Class
                         .forName("etmo.problems.WCCI2020.MATP" + problemId)
+                        .getMethod("getProblem")
+                        .invoke(null, null);
+                break;
+            case CEC2019:
+                problemSet = (ProblemSet) Class
+                        .forName("etmo.problems.CEC2019.MATP" + problemId)
                         .getMethod("getProblem")
                         .invoke(null, null);
                 break;
